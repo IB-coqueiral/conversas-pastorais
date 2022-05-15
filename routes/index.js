d5fce6b9ded61f718b14a9c50ce6c59e2b8e2f47
Dyego Morais <dyego.morais@CLICKBUS0447.local>
2022-05-15 18:05:05 -0300

    route

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('em_breve', { 
-    title: 'III Congresso Miqueias Brasil 2022',
-    layout: 'layout/em_breve' 
-  });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('em_breve', { 
+//     title: 'III Congresso Miqueias Brasil 2022',
+//     layout: 'layout/em_breve' 
+//   });
+// });
 
-router.get('/producao', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('index', { 
     title: 'III Congresso Miqueias Brasil 2022',
     layout: "layout/principal" 
